eb9040893d7794df7bf4ac8a88a91ecae157b7d7
lindseyringwald <lindseyringwald@gmail.com>
2016-09-28 02:31:23 -0400

    Added app and updated functions

--- src/clojure_web/core.clj
+++ src/clojure_web/core.clj
@@ -1,62 +1,42 @@
-(ns clojure-web.core
+app isn't defined. in our class work, we defined app as our route collection.
+Because we have no routes defined, we're not using any of the cool functions you've written to render out the site.
+
+(ns purchases-clojure.core
   (:require  [clojure.string :as str]
              [compojure.core :as c]
              [ring.adapter.jetty :as j]
              [hiccup.core :as h])
   (:gen-class))
 
-(defn parse-purchases []
-  (let [purchases (slurp "purchases.csv")
-        purchases (str/split-lines purchases)
-        purchases (map (fn [line]
-                         (str/split line #",")) purchases)
-        header (first purchases)
-        purchases (rest purchases)
-        purchases (map (fn [line]
-                         (zipmap header line))
-                       purchases)]
-    purchases))
+(c/defroutes app
+             (c/GET "/" []
+               "Hello, world!"))
 
-;customer_id,date,credit_card,cvv,category
-(defn purchases-html [purchases]
-  [:table {:class "table"}
-   [:thead
-    [:tr
-     [:td "Customer ID"]
-     [:td "date"]
-     [:td "Credit Card"]
-     [:td "CVV"]
-     [:td "Category"]
-     ]
-    ]
-   [:tbody
-    (map (fn [purchase]
-           [:tr
-            [:td (get purchase "customer_id")]
-            [:td (get purchase "date")]
-            [:td (get purchase "credit_card")]
-            [:td (get purchase "cvv")]
-            [:td (get purchase "category")]
-            ]
-           )
-         purchases)
-    ]])
+(c/defroutes app
+             (c/GET "/" []
+               (h/html [:html
+                        [:body
+                         [:h2 "Hello, world!"]]])))
 
-;get a category using A/N function
-(defn get-categories []
-  (let [purchases (parse-purchases)
-        categories (set (map #(get % "category") purchases))]
-    categories))
+(defn purchases-html [category]
+  (let [purchases (read-purchases)
+        purchases (if (= 0 (count country))
+           purchases;
+           (filter (fn [purchases]
+                         (= (get purchases "category") category))
+                       purchases))]
+    [:ol
+     (map (fn [purchase]
+            [:li (str (get purchase "customer_id") " " (get purchase "date") " " (get purchase "credit_card") " " (get purchase "csv") " " (get purchase "category")) ])
+          purchases)]))
 
-;add links to filter categories
-(defn nav-html [categories]
-  [:nav {:class "navbar navbar-default"} [:div {:class "container-fluid"}
-                                          [:ul {:class "nav navbar-nav"}
-                                           [:li
-                                            [:a {:href "/"} "All"]]
-                                           (map (fn [category]
-                                                  [:li
-                                                   [:a {:href (str "/" category)} category]]) categories)]]])
+
+(c/defroutes app
+              (c/GET "/:category{.*}" [category]
+                (h/html [:html
+                         [:body
+                          (purchases-html category)]])))
+
 
 ;store state in nil atom
 (defonce server (atom nil))
@@ -65,9 +45,18 @@
 ;jetty returns a server
 ;change it to reset -- to get the value at the specific state in atoms -- call that value @server
 (defn -main []
-  (when @server
-    (.stop @server))
-  ;reset the server value back to a new webserver
-  (reset! server (j/run-jetty app {:port 3000 :join? false})))
+  (reset! server))
+  (reset! server(j/run-jetty app {:port 3000 :join? false})))
 
 
+;Create a project with purchases.csv in it
+;Read the file and parse it into a vector of lines
+;Split each line into vectors (delimited by commas)
+;Separate the header from the rest of the lines
+;Make each line a hash map that associates each item in the header with each item in the line
+;Ask the user to type a category
+;The possible categories are: Furniture, Alcohol, Toiletries, Shoes, Food, Jewelry
+;Read a line of input from the user;
+;Filter the vector so it only contains hash maps whose category equals the input from the user
+;Use the clojure json library to convert the map to json
+;Save the results into a file called filtered_purchases.json
